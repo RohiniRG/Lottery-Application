d5a33dcd941d616f734eb4b9f76413b0db8bb487
Rohini Rao <rohinirao2112@gmail.com>
2022-01-30 18:00:37 +0530

    added lottery manager in card header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import Box from '@mui/material/Box';
 import CustomAppBar from './components/customAppBar';
 import Card from '@mui/material/Card';
 import CustomCardHeader from './components/cardHeader';
+// import { MuiThemeProvider, createTheme, useTheme } from "@mui/material/styles";
+
+// const theme = createTheme({
+//   palette: {
+//     background: {
+//       default: 'lightgrey'
+//     }
+//   }
+// });
 
 class App extends Component {
   state = {
@@ -63,8 +72,9 @@ class App extends Component {
           }}
         >
           <CustomAppBar />
-          <Card>
-            <CustomCardHeader />
+          <Card >
+            <CustomCardHeader manager={this.state.manager}/>
+            <hr color='lightgray'/>
             <p>
               This contract is managed by {this.state.manager}.
               There are currently {this.state.players.length} people already competing for {web3.utils.fromWei(this.state.balance, 'ether')} ethers!!
--- src/components/cardHeader.js
+++ src/components/cardHeader.js
@@ -2,18 +2,28 @@ import { Component } from "react";
 import CardHeader from '@mui/material/CardHeader';
 import Avatar from '@mui/material/Avatar';
 import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 class CustomCardHeader extends Component {
   render() {
     return (
-        <CardHeader
+        <CardHeader 
         avatar={
             <Avatar sx={{ color: 'black', bgcolor: 'white', fontSize: 'large' }}>
               <PersonIcon />
             </Avatar>
           }
+          action={
+            <IconButton sx={{ color: 'black' }}>
+              <MoreVertIcon/>
+            </IconButton>
+          }
           title="Lottery Manager"
-          subheader={this.state.manager}
+          subheader={
+            <a href="https://rinkeby.etherscan.io/address/0x56709884751800A72370F8380671ec816AA4835A">
+              {this.props.manager}
+            </a>}
         />
     );
   }
